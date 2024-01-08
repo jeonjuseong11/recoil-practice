39095a0644c426dfecce75a47e44ff75c5c03c9d
전주성 <jeonjuseong11@gmail.com>
2024-01-08 11:28:53 +0900

    Fixed : tailwind
    tailwind css 적용 안되는 문제 해결

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  purge: [],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
